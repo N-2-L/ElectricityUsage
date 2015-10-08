c38b8752afc95b133484d6082a2951d9a773b872
ShanikaEdiriweera <shanika.13@cse.mrt.ac.lk>
2015-10-08 16:36:12 +0530

    chart + home changes-s

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,5 @@
-//angular.module('electricityUsage.controllers', [])
-angular.module('electricityUsage.controllers', ['ui.bootstrap','countTo'])
+angular.module('electricityUsage.controllers', ['ui.bootstrap','countTo','chart.js'])
+//angular.module('electricityUsage.controllers', ['ui.bootstrap','countTo'])
 
 .controller('DashCtrl', function($scope) {})
 
@@ -54,7 +54,41 @@ angular.module('electricityUsage.controllers', ['ui.bootstrap','countTo'])
       }
     });
 
-  });
+  })
+
+  //// Optional configuration
+  //.config(['ChartJsProvider', function (ChartJsProvider) {
+  //  // Configure all charts
+  //  ChartJsProvider.setOptions({
+  //    colours: ['#FF5252', '#FF8A80'],
+  //    responsive: false
+  //  });
+  //  // Configure all line charts
+  //  ChartJsProvider.setOptions('Line', {
+  //    datasetFill: false
+  //  });
+  //}])
+
+  .controller('LineCtrl', ['$scope', '$timeout', function ($scope, $timeout) {
+
+    $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+    $scope.onClick = function (points, evt) {
+      console.log(points, evt);
+    };
+
+    // Simulate async data update
+    $timeout(function () {
+      $scope.data = [
+        [28, 48, 40, 19, 86, 27, 90],
+        [65, 59, 80, 81, 56, 55, 40]
+      ];
+    }, 3000);
+  }]);
 
 
 
